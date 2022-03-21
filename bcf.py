93dd85e148dc5ac54ee46f6d8a3420078a04e982
Johannes Hitzinger <jonny0815ger@gmail.com>
2022-03-21 12:42:41 +0100

    progress, buttons rapidfire themselves :(

--- bcf.py
+++ bcf.py
@@ -6,6 +6,18 @@ from typing import List
 import Live
 
 
+def translate_color_index(color_index: int) -> int:
+    color_table = {14: 0,  # 14 red
+                   9: 1,  # 9 blue
+                   3: 2,  # 3 yellow
+                   19: 3,  # 19 green
+                   15: 4,  # 15 orange
+                   66: 5,  # 66 purple
+                   13: 6,  # 13 white
+                   16: 7}  # 16 brown
+    return color_table[color_index]
+
+
 class Bcf(ControlSurfaceComponent):
     def __init__(self, *a, **k):
         super(Bcf, self).__init__(*a, **k)
@@ -17,7 +29,7 @@ class Bcf(ControlSurfaceComponent):
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 70),
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 71),
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 72)],
-                                                                [ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 73),
+                                                                [ConfigurableButtonElement(True, MIDI_CC_TYPE, 8, 73),
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 74),
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 75),
                                                                  ConfigurableButtonElement(False, MIDI_CC_TYPE, 8, 76),
@@ -65,6 +77,15 @@ class Bcf(ControlSurfaceComponent):
                                                     EncoderElement(MIDI_CC_TYPE, 8, 87, Live.MidiMap.MapMode.absolute),
                                                     EncoderElement(MIDI_CC_TYPE, 8, 88, Live.MidiMap.MapMode.absolute),
                                                     EncoderElement(MIDI_CC_TYPE, 8, 89, Live.MidiMap.MapMode.absolute)]]
+
+    def get_volume_control(self, clip: Live.Clip.Clip) -> EncoderElement:
+        return self.faders[translate_color_index(clip.color_index)]
+
+    def get_arm_button(self, clip: Live.Clip.Clip) -> ConfigurableButtonElement:
+        return self._buttons[1][translate_color_index(clip.color_index)]
+
+    def get_cue_button(self, clip: Live.Clip.Clip) -> ConfigurableButtonElement:
+        return self._buttons[0][translate_color_index(clip.color_index)]
 
     def on_selected_scene_changed(self):
         self.canonical_parent.show_message("Hello from bcf")
--- mdcr.py
+++ mdcr.py
@@ -34,7 +34,15 @@ class Mdcr(ControlSurface):
             self.current_scene_offset = 0
             num_tracks = 8
             num_returns = 2
-            self._active_tracks: Dict[int, Live.Track.Track] = {}
+            self._active_tracks: Dict[int, Live.Track.Track] = {14: None,  # 14 red
+                                                                9: None,  # 9 blue
+                                                                3: None,  # 3 yellow
+                                                                19: None,  # 19 green
+                                                                15: None,  # 15 orange
+                                                                66: None,  # 66 purple
+                                                                13: None,  # 13 white
+                                                                16: None # 16 brown
+                                                                }
             self.mixer = MixerComponent(num_tracks, num_returns, is_enabled=True, auto_name=True)
             self.session = SessionComponent(num_tracks, 4)
             self.session.set_offsets(0, 0)
@@ -43,26 +51,53 @@ class Mdcr(ControlSurface):
             self.song().view.selected_track = self.song().tracks[0]
             self._bcf: Bcf = Bcf(self)
             self._bcr: Bcr = Bcr(self)
-            #self._fcb: Fcb = Fcb(self)
+            # self._fcb: Fcb = Fcb(self)
             # self.mixer.channel_strip(0).set_volume_control(EncoderElement(MIDI_CC_TYPE, 8, 1, Live.MidiMap.MapMode.absolute))
-            self.mixer.channel_strip(0).set_volume_control(self._bcf.faders[0])
+            #self.mixer.channel_strip(0).set_volume_control(self._bcf.faders[0])
+            self.mixer.channel_strip(0).set_arm_button(self._bcf._buttons[1][0])
             self.show_message("did it?")
             self.set_highlighting_session_component(self.session)
             self.init_clip_listeners()
-
+            self.show_message(str(type(self.song().tracks)))
+            self.log_message(str(type(self.song().tracks)))
 
     def init_clip_listeners(self):
         song: Live.Song.Song = self.song()
         for i in range(len(song.tracks)):
             for j in range(len(song.tracks[i].clip_slots)):
                 if song.tracks[i].clip_slots[j].has_clip:
+                    # song.tracks[i].clip_slots[j].clip.add_playing_status_listener(self.handle_clip_launch)
                     song.tracks[i].clip_slots[j].clip.add_playing_status_listener(
                         partial(self.handle_clip_launch, song.tracks[i].clip_slots[j].clip))
 
     def handle_clip_launch(self, clip: Live.Clip.Clip):
-        self.show_message("test + " + str(clip.color_index))
-        self._active_tracks[clip.color_index] = clip.canonical_parent.canonical_parent
-        self.show_message("track: " + str(self._active_tracks[clip.color_index]))
+        self.show_message("hello from handle clip launch: " + str(clip.color_index))
+        track: Live.Track.Track = clip.canonical_parent.canonical_parent
+        if clip.color_index in self._active_tracks:  # anything wrong with clip coloring?
+            if self._active_tracks[clip.color_index] == track:  # same clip? return
+                self.show_message("returing wrong")
+                return
+            else:
+
+                self._active_tracks[clip.color_index] = track
+                index: int = self.get_track_index(track)
+                self.show_message("trying to map track " + str(index) + " to " + str(track) )
+                self.log_message("trying to map track " + str(index) + " to " + str(track))
+                with self.component_guard():
+                    self.mixer.channel_strip(index).set_volume_control(
+                        self._bcf.get_volume_control(clip))
+                    #self.mixer.channel_strip(index).set_arm_button(self._bcf.get_arm_button(clip))
+                    #self.mixer.channel_strip(index).set_solo_button(self._bcf.get_cue_button(clip))
+
+        self.show_message(
+            "track: " + str(self._active_tracks[clip.color_index]) + " has color: " + str(clip.color_index))
+        # self.show_message("test")
+
+    def get_track_index(self, track: Live.Track.Track) -> int:
+        tracks = self.song().tracks
+        for i in range(len(tracks)):
+            if tracks[i] == track:
+                return i
 
     def disconnect(self):
         super(Mdcr, self).disconnect()
